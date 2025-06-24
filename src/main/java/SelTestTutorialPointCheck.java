eb73a699ccbd4ced5d7f7d9b47bb67816bf572a0
AdamVasina <adamvasina@gmail.com>
2025-06-24 20:33:03 +0200

    init commit

---
+++ src/main/java/SelTestTutorialPointCheck.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+
+public class SelTestTutorialPointCheck {
+
+    private WebDriver ovladac;
+
+    @BeforeEach
+    public void  setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\adamv\\Desktop\\Selenium\\src\\main\\resources\\chromedriver.exe");
+
+
+
+
+    }
+
+}
