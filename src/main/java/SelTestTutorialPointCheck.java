e88a23b16fb956bf0c6f6d607159b56e548f8894
AdamVasina <adamvasina@gmail.com>
2025-06-24 20:56:00 +0200

    init commit

--- src/main/java/SelTestTutorialPointCheck.java
+++ src/main/java/SelTestTutorialPointCheck.java
@@ -1,5 +1,14 @@
+import org.jspecify.annotations.Nullable;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
 
 
 public class SelTestTutorialPointCheck {
@@ -9,10 +18,32 @@ public class SelTestTutorialPointCheck {
     @BeforeEach
     public void  setUp(){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\adamv\\Desktop\\Selenium\\src\\main\\resources\\chromedriver.exe");
+        ovladac = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        ovladac.quit();
+    }
 
 
+    @Test
+    public void CheckBoxTest(){
+        //Prepnutie na stranku
+        ovladac.get("https://www.tutorialspoint.com/selenium/practice/check-box.php");
+
+        //Najdenie objektu
+        ovladac.findElement(By.xpath("//*[@id=\"bs_1\"]/span[1]")).click();
+
+        //Kliknutie na checkboxy
+        ovladac.findElement(By.id("c_bf_1")).click();
+        ovladac.findElement(By.id("c_bf_2")).click();
 
 
     }
 
+
+
 }
+
+
