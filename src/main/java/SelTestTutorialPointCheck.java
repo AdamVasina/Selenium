0b8ab8b1466904572543f67a0efbdf1283cf1789
AdamVasina <adamvasina@gmail.com>
2025-06-26 19:17:30 +0200

    init commit

--- src/main/java/SelTestTutorialPointCheck.java
+++ src/main/java/SelTestTutorialPointCheck.java
@@ -1,11 +1,15 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 import java.util.List;
 import java.util.Set;
@@ -19,6 +23,11 @@ public class SelTestTutorialPointCheck {
     public void  setUp(){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\adamv\\Desktop\\Selenium\\src\\main\\resources\\chromedriver.exe");
         ovladac = new ChromeDriver();
+
+        //Alternativne nastavenie - Automaticke sprava webdriveru
+        //WebDriverManager.chromedriver().setup();
+        //ovladac = new ChromeDriver();
+
     }
 
     @AfterEach
@@ -38,11 +47,14 @@ public class SelTestTutorialPointCheck {
         //Kliknutie na checkboxy
         ovladac.findElement(By.id("c_bf_1")).click();
         ovladac.findElement(By.id("c_bf_2")).click();
+        //Kontrola ci je hlavne pole zaskrtnute
+        assertTrue(ovladac.findElement(By.id("c_bs_1")).isSelected());
+        ovladac.findElement(By.id("c_bf_1")).click();
+        //Kontrola ci je hlavne pole odskrtnute
+        assertFalse(ovladac.findElement(By.id("c_bs_1")).isSelected());
 
 
     }
-
-
 
 }
 
